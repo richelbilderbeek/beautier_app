30b84d956459b588255850ec8aaa296e886793b9
richelbilderbeek <richel@richelbilderbeek.nl>
2018-08-11 18:01:42 +0200

    First response

--- app.R
+++ app.R
@@ -3,16 +3,23 @@ library(babette)
 
 # Define user interface
 ui <- fluidPage(
-   shinyUI(
-    navbarPage("beautier",
-      tabPanel("Partitions",
-        textInput("text", h3("Filename"), value = "anthus_aco.fas")
+  shinyUI(
+    sidebarLayout(
+      sidebarPanel = navbarPage(
+        title = "beautier",
+        tabPanel(
+          title = "Partitions",
+          textInput("filename", h3("Filename"), value = "anthus_aco.fas")
+        ),
+        tabPanel("Tip Dates"),
+        tabPanel("Site Model"),
+        tabPanel("Clock Model"),
+        tabPanel("Priors"),
+        tabPanel("MCMC")
       ),
-      tabPanel("Tip Dates"),
-      tabPanel("Site Model"),
-      tabPanel("Clock Model"),
-      tabPanel("Priors"),
-      tabPanel("MCMC")
+      mainPanel(
+        textOutput("result")
+      )
     )
   )
 )
@@ -20,14 +27,10 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  output$result <- renderText({ 
+    paste("Filename:", input$filename)
+  })
+   
 }
 
 # Run the application 
