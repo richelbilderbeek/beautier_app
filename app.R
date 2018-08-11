bf4be3bab8b6635c982ae62d4400b96a93fcef9c
richelbilderbeek <richel@richelbilderbeek.nl>
2018-08-11 19:38:34 +0200

    Also evaluate the beautier command

--- app.R
+++ app.R
@@ -1,5 +1,5 @@
 library(shiny)
-library(babette)
+library(beautier)
 
 # Define user interface
 ui <- fluidPage(
@@ -18,7 +18,9 @@ ui <- fluidPage(
           numericInput("prop_invariant", "Proportion invariant", value = 0.0, min = 0.0, max = 1.0),
           selectInput("subst_model", "Site model", choices = c("JC69", "HKY", "TN93", "GTR"))
         ),
-        tabPanel("Clock Model"),
+        tabPanel("Clock Model",
+          selectInput("clock_model", "Clock model", choices = c("Strict Clock", "Relaxed Clock Log Normal"))
+        ),
         tabPanel("Priors"),
         tabPanel("MCMC")
       ),
@@ -36,35 +38,54 @@ ui <- fluidPage(
 
 create_site_model_text <- function(subst_model) {
   if (subst_model == "JC69") {
-    return ("create_jc69_site_model()")
+    return("create_jc69_site_model()")
   }
   if (subst_model == "HKY") {
-    return ("create_hky_site_model()")
+    return("create_hky_site_model()")
   }
   if (subst_model == "TN93") {
-    return ("create_tn93_site_model()")
+    return("create_tn93_site_model()")
   }
   if (subst_model == "GTR") {
-    return ("create_gtr_site_model()")
+    return("create_gtr_site_model()")
   }
+}
+
+create_clock_model_text <- function(clock_model) {
+  if (clock_model == "Strict Clock") {
+    return ("create_strict_clock_model()")
+  }
+  if (clock_model == "Relaxed Clock Log Normal") {
+    return ("create_rln_clock_model()")
+  }
+}
+
+create_beautier_cmd <- function(input) {
+  paste(
+    "create_beast2_input(",
+    paste0("  input_filenames = \"", input$filename, "\","),
+    paste0("  site_models = ", create_site_model_text(input$subst_model), ","),
+    paste0("  clock_models = ", create_clock_model_text(input$clock_model)),
+    ")",
+    sep = "\n", collapse = "\n"
+  )  
+}
+create_xml <- function(beautier_cmd) {
+  paste(
+    eval(parse(text = paste(beautier_cmd, collapse = ""))),
+    collapse = "\n"
+  )
 }
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
   output$result <- renderText({ 
-    paste(
-      "library(beautier)",
-      "create_beast2_input(",
-      paste0("  input_filenames = \"", input$filename, "\","),
-      paste0("  site_models = ", create_site_model_text(input$subst_model)),
-      ")",
-      sep = "\n", collapse = "\n"
-    )
+    create_beautier_cmd(input)  
   })
 
   output$xml <- renderText({
-    paste(create_beast2_input(input$filename), collapse = "\n")
+    create_xml(create_beautier_cmd(input))
   })
    
 }
